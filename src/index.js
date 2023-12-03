757e5b4010589560b6915f0aea9d99a8dd68483f
SerhiiRud <carmagsales@gmail.com>
2023-12-03 22:31:55 +0200

    url deploy correction

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/car-rental-app">
+    <BrowserRouter basename="/car-rental-app-ts">
       <App />
     </BrowserRouter>
   </React.StrictMode>
