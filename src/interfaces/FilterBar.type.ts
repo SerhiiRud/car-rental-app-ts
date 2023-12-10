6736eaf9593141ce04482208ecd4e52af4fbab95
SerhiiRud <carmagsales@gmail.com>
2023-12-10 17:35:52 +0200

    favs state corrected

--- src/interfaces/FilterBar.type.ts
+++ src/interfaces/FilterBar.type.ts
@@ -1,6 +1,6 @@
 import { TCar } from "./Car.type";
 
 export type TFilterBar = {
-  setVisibleCars?: React.Dispatch<React.SetStateAction<TCar[]>>;
+  setVisibleCars: React.Dispatch<React.SetStateAction<TCar[]>>;
   cars: TCar[];
 };
--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
 import FilterBar from "../../components/FilterBar/FilterBar";
 import Gallery from "../../components/Gallery";
 import { Container, Text } from "./Favorites.styled";
+import { TCar } from "../../interfaces/Car.type";
 import { TFavoritesPage } from "../../interfaces/FavoritesPage.type";
 
 const Favorites = ({ cars, favoriteToggle }: TFavoritesPage) => {
-  //const [visibleCars, setVisibleCars] = useState<TCar[]>([]);
+  const [visibleCars, setVisibleCars] = useState<TCar[]>([]);
 
-  const visibleCars = localStorage.getItem("favs")
+  const favoriteCars = localStorage.getItem("favs")
     ? JSON.parse(localStorage.getItem("favs") || "{}")
     : [];
 
   return (
     <Container>
-      <FilterBar cars={cars} />
+      <FilterBar cars={favoriteCars} setVisibleCars={setVisibleCars} />
       {visibleCars.length === 0 ? (
         <Text>You don't have favorite cars</Text>
       ) : (
